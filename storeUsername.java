aa81cfeed9d5ab1e94941f6af6ec2947c0be4555
b9041117 <77241868+b9041117@users.noreply.github.com>
2021-01-11 13:31:03 +0000

    Add files via upload

---
+++ storeUsername.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var Username = document.getElementById("Username").value;
+        var Password = document.getElementById("Password").value;
+
+    }
+}
