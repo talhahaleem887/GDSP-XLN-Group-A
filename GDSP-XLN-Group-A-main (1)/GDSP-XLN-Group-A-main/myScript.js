6480f402eb3e40c4a8202f5cd575cc52d9ee72ae
chrisF20 <72350859+chrisF20@users.noreply.github.com>
2021-01-14 13:11:02 +0000

    Add files via upload

---
+++ GDSP-XLN-Group-A-main (1)/GDSP-XLN-Group-A-main/myScript.js
@@ -0,0 +1,28 @@
+
+function showHide(shown, hidden){
+    document.getElementById(hidden).style.display='block';
+    document.getElementById(shown).style.display='none';
+    return false;
+}
+
+function show(hidden){
+    document.getElementById(hidden).style.display='block';
+    return false;
+}
+
+// function check(form)
+// {
+
+// if(form.document == "landLineStart" && form.getElementById == "ogY")
+// {
+
+//     var myWindow = location.replace("outgoingOnly.html");
+
+//     return true;
+// }
+// else if(form.document == "landLineStart" && form.getElementById == "ogN")
+// {
+//     var myWindow = location.replace("testSocket.html");
+//     return true;
+// }
+// }
